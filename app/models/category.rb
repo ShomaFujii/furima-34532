4de31508fb745a19e9ecfde35a21af2035603c98
ShomaFujii <soccer.11.sho-ma@i.softbank.jp>
2021-02-24 18:18:30 +0900

    ActiveStorageの導入

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+class category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'レディース' },
+    { id: 3, name: 'メンズ' },
+    { id: 4, name: 'ベビー・キッズ' },
+    { id: 5, name: 'インテリア・住まい・小物' },
+    { id: 6, name: '本・音楽・ゲーム' },
+    { id: 7, name: '全体的に状態が悪い' },
+  ]
+ 
+
+  include ActiveHash::Associations
+  has_many :items
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+belongs_to :user
+has_one_attached :image
+
+with_options presence: true do
+  validates :name
+  validates :discribe
+  validates :price
+end
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+
+  validates :genre_id, numericality: { other_than: 1 }
+  
 end
