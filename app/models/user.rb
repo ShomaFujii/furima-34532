8d2cafa7e5919b64d2e86bf31020249efdda53ef
ShomaFujii <soccer.11.sho-ma@i.softbank.jp>
2021-02-24 14:14:40 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
         
          validates :nickname, presence: true
          validates :first_name_character, presence: true,
-         format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+         format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message:"Full-width characters" }
          validates :last_name_character, presence: true,
-         format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+         format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message:"Full-width characters"}
          validates :first_name_kana, presence: true,
-         format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+         format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "Full-width katakana characters" }
          validates :last_name_kana, presence: true,
-         format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+         format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message:"Full-width katakana characters" }
          validates :birthday, presence: true
+
+         validates :password,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ ,message: " Include both letters and numbers"}
 
 end
 
